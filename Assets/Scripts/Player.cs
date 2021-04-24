b3ed7a5c493c2d5c1266f99baf8f75122c161b43
grizz03 <sawilson1110@yahoo.com>
2021-04-24 14:08:50 -0700

    updated boundary

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,10 @@ public class Player : MonoBehaviour
 
     [SerializeField] float movementSpeed = 10f;
 
-    float xMin;
-    float xMax;
-
     // Start is called before the first frame update
     void Start()
     {
-        // SetUpNewBoundary();
+    
     }
 
     // Update is called once per frame
@@ -28,16 +25,9 @@ public class Player : MonoBehaviour
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
         //set boundaries of movement
-        float newXPos = Mathf.Clamp(transform.position.x + deltaX, -6.2f, 6.2f);
-        float newYPos = Mathf.Clamp(transform.position.y + deltaX, -8.6f, 8.6f);
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, -6.1f, 6.1f);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, -8.6f, 8.6f);
 
         transform.position = new Vector2(newXPos, newYPos);
     }
-
-    // private void SetUpNewBoundary()
-    // {
-    //     Camera gameCamera = Camera.main;
-    //     xMin = gameCamera.ViewportToScreenPoint(new Vector3(0, 0, 0)).x;
-    //     xMax = gameCamera.ViewportToScreenPoint(new Vector3(1, 0, 0)).x;
-    // }
 }
