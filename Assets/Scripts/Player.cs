654a00634ee4f351909a59db2f5d11aa7e354111
grizz03 <sawilson1110@yahoo.com>
2021-04-24 14:04:58 -0700

    made player boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,13 @@ public class Player : MonoBehaviour
 
     [SerializeField] float movementSpeed = 10f;
 
+    float xMin;
+    float xMax;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // SetUpNewBoundary();
     }
 
     // Update is called once per frame
@@ -20,11 +24,20 @@ public class Player : MonoBehaviour
     }
 
     private void Movement()
-    {
+    {   //create movement and fps independent
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
-        float newXPos = transform.position.x + deltaX;
-        float newYPos = transform.position.y + deltaY;
+        //set boundaries of movement
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, -6.2f, 6.2f);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaX, -8.6f, 8.6f);
+
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+    // private void SetUpNewBoundary()
+    // {
+    //     Camera gameCamera = Camera.main;
+    //     xMin = gameCamera.ViewportToScreenPoint(new Vector3(0, 0, 0)).x;
+    //     xMax = gameCamera.ViewportToScreenPoint(new Vector3(1, 0, 0)).x;
+    // }
 }
