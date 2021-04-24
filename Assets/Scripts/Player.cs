d7ba7728cf3a50f151a784c490f5fc302e3228af
grizz03 <sawilson1110@yahoo.com>
2021-04-24 13:37:31 -0700

    added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] float movementSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        float newXPos = transform.position.x + deltaX;
+        float newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }
